ba92f08981e4bbb96ed196625e7c99190686185f
Dirk Kiesewetter <dirk.k1@outlook.com>
2018-12-25 10:38:21 -0500

    initial commit

---
+++ config/orm.js
@@ -0,0 +1,11 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+function SelectAll();
+
+function insertOne();
+
+function updateOne();
+
+
+module.exports = orm;
