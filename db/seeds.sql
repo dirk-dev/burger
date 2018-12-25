776e71d2f2d77328c1b0e4b34a0153d597fcefbb
Dirk Kiesewetter <dirk.k1@outlook.com>
2018-12-25 08:14:59 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('The Baconator');
+INSERT INTO burgers (burger_name) VALUES ('The Nasty Soy Burger');
+INSERT INTO burgers (burger_name) VALUES ('The BBQ Bacon Deluxe');
+
+
+
